23dcfd6637319a4fa40c9426237d071ab0bcda9e
Pedro_Verillo <112446809+Verillo27@users.noreply.github.com>
2025-02-26 19:50:59 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.button("botao salvar")
 st.header("cabeçalho")
 st.toggle("ligar se for elu/delu")
+st.text_input("movie title", "life of brian")
+st.text_area("enter text")
