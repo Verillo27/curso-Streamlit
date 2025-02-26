7fb9abc2ecc648f58b9ed430347d8b2fb0c132f0
Pedro_Verillo <112446809+Verillo27@users.noreply.github.com>
2025-02-26 20:30:16 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -16,11 +16,11 @@ st.checkbox("Sorvete")
 st.checkbox("refri")
 st.color_picker("pick a color", "#00F900")
 st.feedback("stars")
-df = pd.dataframe(
+df = pd.DataFrame(
   [
     {"command" : "st.selectbox", "rating" : 4,"is_widget" : True},
-     {"command" : "st.ballons", "rating" : 5,"is_widget" : False},
-     {"command" : "st.time_input", "rating" : 3,"is_widget" : True},
+    {"command" : "st.ballons", "rating" : 5,"is_widget" : False},
+    {"command" : "st.time_input", "rating" : 3,"is_widget" : True},
   ]
 )
 edited_df = st.data_editor(df)
