c4e38ecfcd35db2a334945a658b8d38b8186c6e3
Pedro_Verillo <112446809+Verillo27@users.noreply.github.com>
2025-02-28 20:04:25 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,11 +1,15 @@
 import streamlit as st
 st.header("CPF")
 st.text_input("")
+
 st.header("Nome")
 st.text_input("")
+
 st.header("Nascimento")
 st.text_input("")
+
 st.header("email")
 st.text_input("")
+
 st.header("telefone")
 st.text_input("")
