f09842b388e31a5256a4d0303795e1495179ab09
Pedro_Verillo <112446809+Verillo27@users.noreply.github.com>
2025-02-28 20:40:12 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import datetime
+
 st.header("CPF")
 st.text_input("", key="input_1")
 st.header("nome")
 st.text_input("", key="input_2")
+st.header("nascimento")
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+st.write("Your birthday is:", d)
 st.header("email")
 st.text_input("", key="input_3")
 st.header("telefone")
