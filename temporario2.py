7fb5690e8193226a903f27524a24a6f92e7f084c
Pedro_Verillo <112446809+Verillo27@users.noreply.github.com>
2025-03-28 19:48:13 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -10,8 +10,10 @@ d = st.date_input("Qual é seu aniversario",
                   format="DD.MM.YYYY")
 st.title("Estado Civil")
 opcoes = ["Solteiro", "Casado", "Divorciado", "Viuvo"]
+escolha = st.selectbox("Selecione uma opção:", opcoes)
 st.title("Escolha uma opção")
 opcoes = ["Mulher", "Homem"]
+escolha = st.selectbox("Selecione uma opção:", opcoes)
 st.header("Salario")
 st.text_input("", key="input_5")
 
