6ca6209733c34790e0fc1a26d004d111504ce7e9
Pedro_Verillo <112446809+Verillo27@users.noreply.github.com>
2025-03-28 19:40:40 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import datetime
 
-st.header("CPF")
+st.header("Nome")
 st.text_input("", key="input_1")
-st.header("nome")
+st.header("Sobrenome")
 st.text_input("", key="input_2")
 st.header("nascimento")
 d = st.date_input("Qual é seu aniversario", 
                   format="DD.MM.YYYY")
-st.header("email")
+st.header("Estado Civil")
 st.text_input("", key="input_3")
-st.header("telefone")
+st.header("Sexo")
 st.text_input("", key="input_4")
+st.header("Salario")
+st.text_input("", key="input_5")
