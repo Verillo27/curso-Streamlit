1bdd9455c5d1a0d627dcdc0d2c558145ba02e5ef
Pedro_Verillo <112446809+Verillo27@users.noreply.github.com>
2025-04-02 20:08:04 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -16,14 +16,14 @@ opcoes = ["Mulher", "Homem"]
 escolha = st.selectbox("Selecione uma opção:", opcoes)
 
 
-# Função para calcular aumento
+
 def calcular_aumento(salario):
     if salario < 2500:
         return "aumento"
     else:
         return "não aumento"
 
-# Classe para gerenciar aumento
+
 class Salario:
     def __init__(self, aumento=0):
         self.aumento = aumento
@@ -34,9 +34,7 @@ class Salario:
             st.write("Aumento de salário:", salario)
             salario += self.aumento
 
-salario = st.number_input("Digite o salário:", min_value=0)
-resultado = calcular_aumento(salario)
-st.write("Resultado:", resultado)
+
 
 salario_obj = Salario()
 salario_obj.ajustar_salario(salario)
